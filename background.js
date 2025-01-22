cda1ad11c3761311a5f1c2a40a064ee6905efdfe
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2025-01-23 00:51:34 +0530

    adding features

--- background.js
+++ background.js
@@ -1,31 +1,37 @@
-let refreshInterval = {};
+let refreshIntervals = {};
 
-chrome.storage.local.get("interval", (data) => {
-  refreshInterval = data.interval || {};
+chrome.storage.local.get("intervals", (data) => {
+  refreshIntervals = data.intervals || {};
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "start") {
-    startRefreshing(request.tabId, request.interval);
+    startRefreshing(request.tabId, request.interval, request.hardRefresh);
   } else if (request.action === "stop") {
     stopRefreshing(request.tabId);
+  } else if (request.action === "globalStart") {
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach(tab => startRefreshing(tab.id, request.interval, request.hardRefresh));
+    });
+  } else if (request.action === "globalStop") {
+    Object.keys(refreshIntervals).forEach(tabId => stopRefreshing(parseInt(tabId)));
   }
 });
 
-function startRefreshing(tabId, interval) {
-  if (refreshInterval[tabId]) {
-    clearInterval(refreshInterval[tabId]);
+function startRefreshing(tabId, interval, hardRefresh) {
+  if (refreshIntervals[tabId]) {
+    clearInterval(refreshIntervals[tabId]);
   }
-  refreshInterval[tabId] = setInterval(() => {
-    chrome.tabs.reload(tabId);
+  refreshIntervals[tabId] = setInterval(() => {
+    chrome.tabs.reload(tabId, { bypassCache: hardRefresh });
   }, interval * 1000);
-  chrome.storage.local.set({ interval: refreshInterval });
+  chrome.storage.local.set({ intervals: refreshIntervals });
 }
 
 function stopRefreshing(tabId) {
-  if (refreshInterval[tabId]) {
-    clearInterval(refreshInterval[tabId]);
-    delete refreshInterval[tabId];
-    chrome.storage.local.set({ interval: refreshInterval });
+  if (refreshIntervals[tabId]) {
+    clearInterval(refreshIntervals[tabId]);
+    delete refreshIntervals[tabId];
+    chrome.storage.local.set({ intervals: refreshIntervals });
   }
 }
--- popup.js
+++ popup.js
@@ -1,21 +1,56 @@
 // popup.js
 document.getElementById("start").addEventListener("click", () => {
     let interval = document.getElementById("interval").value;
+    let hardRefresh = document.getElementById("hardRefresh").checked;
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.runtime.sendMessage({
         action: "start",
         tabId: tabs[0].id,
-        interval: parseInt(interval)
+        interval: parseInt(interval),
+        hardRefresh: hardRefresh
       });
+      updateActiveTabs();
     });
   });
   
   document.getElementById("stop").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.runtime.sendMessage({
-        action: "stop",
-        tabId: tabs[0].id
-      });
+      chrome.runtime.sendMessage({ action: "stop", tabId: tabs[0].id });
+      updateActiveTabs();
     });
   });
+  
+  document.getElementById("globalStart").addEventListener("click", () => {
+    let interval = document.getElementById("interval").value;
+    let hardRefresh = document.getElementById("hardRefresh").checked;
+    chrome.runtime.sendMessage({ action: "globalStart", interval: parseInt(interval), hardRefresh: hardRefresh });
+    updateActiveTabs();
+  });
+  
+  document.getElementById("globalStop").addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "globalStop" });
+    updateActiveTabs();
+  });
+  
+  function updateActiveTabs() {
+    chrome.storage.local.get("intervals", (data) => {
+      let activeTabs = data.intervals || {};
+      let list = document.getElementById("activeTabs");
+      list.innerHTML = "";
+      Object.keys(activeTabs).forEach(tabId => {
+        let listItem = document.createElement("li");
+        listItem.textContent = "Tab ID: " + tabId;
+        let stopButton = document.createElement("button");
+        stopButton.textContent = "✖";
+        stopButton.addEventListener("click", () => {
+          chrome.runtime.sendMessage({ action: "stop", tabId: parseInt(tabId) });
+          updateActiveTabs();
+        });
+        listItem.appendChild(stopButton);
+        list.appendChild(listItem);
+      });
+    });
+  }
+  
+  updateActiveTabs();
   
